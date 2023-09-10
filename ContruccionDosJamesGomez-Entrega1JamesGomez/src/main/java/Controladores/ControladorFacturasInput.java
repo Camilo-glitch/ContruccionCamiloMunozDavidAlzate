b5143c3036df915745af6bcae75cbdaf150b6704
Camilo-glitch <141791864+Camilo-glitch@users.noreply.github.com>
2023-09-10 04:28:01 -0500

    Delete ContruccionDosJamesGomez-Entrega1JamesGomez directory

--- ContruccionDosJamesGomez-Entrega1JamesGomez/src/main/java/Controladores/ControladorFacturasInput.java
+++
@@ -1,6 +0,0 @@
-package Controladores;
-
-public interface ControladorFacturasInput {
-	public String validarConceptoConsumo (String nombre) throws Exception;
-	public double validarValor (String fondos) throws Exception;	
-}
